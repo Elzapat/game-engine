053fffd59dd1ccc90e5e821cc7a38f5b993efc95
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-09-12 10:24:14 -0400

    init première phase

---
+++ include/particule.hpp
@@ -0,0 +1,6 @@
+#ifndef PARTICULE_HPP
+#define PARTICULE_HPP
+
+class Particule {};
+
+#endif // PARTICULE_HPP
---
+++ include/vector3D.hpp
@@ -0,0 +1,6 @@
+#ifndef VECTOR3D_HPP
+#define VECTOR3D_HPP
+
+class Vector3D {};
+
+#endif  // VECTOR3D_HPP
