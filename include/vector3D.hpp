2c151bdea4211262636f1328bb12c099587a6271
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-09-12 10:40:25 -0400

    Vector3D class

--- include/vector3D.hpp
+++ include/vector3D.hpp
@@ -1,6 +1,25 @@
 #ifndef VECTOR3D_HPP
 #define VECTOR3D_HPP
 
-class Vector3D {};
+class Vector3D {
+    private:
+        float x, y, z;
 
-#endif  // VECTOR3D_HPP
+    public:
+        Vector3D(float x = 0.0, float y = 0.0, float z = 0.0) noexcept;
+        ~Vector3D();
+
+        void set_x(float x) noexcept;
+        void set_y(float y) noexcept;
+        void set_z(float z) noexcept;
+
+        void get_x() const noexcept;
+        void get_y() const noexcept;
+        void get_z() const noexcept;
+
+        Vector3D operator+(Vector3D rhs) const noexcept;
+        Vector3D operator-(Vector3D rhs) const noexcept;
+        Vector3D operator*(float rhs) const noexcept;
+};
+
+#endif // VECTOR3D_HPP
