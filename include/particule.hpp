5df69389397590ddfb5d967babfbbbcd1427d7ff
T0mGib <113405723+T0mGib@users.noreply.github.com>
2022-09-19 08:10:11 -0400

    Ajout integration et defs header particules

--- include/particule.hpp
+++ include/particule.hpp
@@ -1,6 +1,35 @@
 #ifndef PARTICULE_HPP
 #define PARTICULE_HPP
 
-class Particule {};
+#include "vector3D.hpp"
+
+/*
+* Class representing physical particule
+*/
+class Particule {
+
+    private:
+        Vector3D position, velocity, acceleration;
+        float inv_mass, damping;
+
+    public:
+        Particule();
+        ~Particule();
+
+        void set_position(Vector3D const& position);
+        void set_velocity(Vector3D const& velocity);
+        void set_acceleration(Vector3D const& acceleration);
+        void set_inv_mass(float const& inv_mass);
+        void set_damping(float const& damping);
+
+        Vector3D get_position();
+        Vector3D get_velocity();
+        Vector3D get_acceleration();
+        float get_inv_mass();
+        float get_damping();
+
+        void integrate(int time_delta);
+
+};
 
 #endif // PARTICULE_HPP
--- src/particule.cpp
+++ src/particule.cpp
@@ -1,1 +1,10 @@
 #include "../include/particule.hpp"
+
+Particule::Particule() {}
+
+void Particule::integrate(int time_delta) {
+
+    this->position = (this->acceleration)*pow(time_delta,2)*0.5 + this->velocity*time_delta + this->position;
+    this->velocity = (this->acceleration)*time_delta + this->velocity;
+
+}
