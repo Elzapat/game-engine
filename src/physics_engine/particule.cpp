9a737b032c2369d01aabe4a7d63a95a355187b77
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-10-03 10:27:54 -0400

    change particule to particle

--- src/physics_engine/particule.cpp
+++ src/physics_engine/particule.cpp
@@ -1,13 +1,13 @@
 #include "../../include/physics_engine/particule.hpp"
 
-Particule::Particule() :
+Particle::Particle() :
     position(math::Vector3D()),
     velocity(math::Vector3D()),
     acceleration(math::Vector3D()),
     inv_mass(0.0f),
     damping(0.9999f) {}
 
-Particule::Particule(
+Particle::Particle(
     math::Vector3D pos,
     math::Vector3D vel,
     math::Vector3D accel,
@@ -20,49 +20,49 @@ Particule::Particule(
     inv_mass(_inv_mass),
     damping(_damping) {}
 
-Particule::~Particule() {}
+Particle::~Particle() {}
 
-void Particule::set_position(math::Vector3D const& position) {
+void Particle::set_position(math::Vector3D const& position) {
     this->position = position;
 }
 
-void Particule::set_velocity(math::Vector3D const& velocity) {
+void Particle::set_velocity(math::Vector3D const& velocity) {
     this->velocity = velocity;
 }
 
-void Particule::set_acceleration(math::Vector3D const& acceleration) {
+void Particle::set_acceleration(math::Vector3D const& acceleration) {
     this->acceleration = acceleration;
 }
 
-void Particule::set_inv_mass(float const& inv_mass) {
+void Particle::set_inv_mass(float const& inv_mass) {
     this->inv_mass = inv_mass;
 }
 
-void Particule::set_damping(float const& damping) {
+void Particle::set_damping(float const& damping) {
     this->damping = damping;
 }
 
-math::Vector3D Particule::get_position() const {
+math::Vector3D Particle::get_position() const {
     return this->position;
 }
 
-math::Vector3D Particule::get_velocity() const {
+math::Vector3D Particle::get_velocity() const {
     return this->velocity;
 }
 
-math::Vector3D Particule::get_acceleration() const {
+math::Vector3D Particle::get_acceleration() const {
     return this->acceleration;
 }
 
-float Particule::get_inv_mass() const {
+float Particle::get_inv_mass() const {
     return this->inv_mass;
 }
 
-float Particule::get_damping() const {
+float Particle::get_damping() const {
     return this->damping;
 }
 
-void Particule::integrate(float dt) {
+void Particle::integrate(float dt) {
     this->position += this->velocity * dt;
     this->velocity += (this->velocity * this->damping - this->velocity) * dt;
     this->velocity += this->acceleration * dt;
--- src/physics_engine/physic_world.cpp
+++ src/physics_engine/physic_world.cpp
@@ -6,14 +6,14 @@ void PhysicWorld::update(float dt) {
     }
 }
 
-void PhysicWorld::add_particule(const Particule particule) {
+void PhysicWorld::add_particule(const Particle particule) {
     this->particules.push_back(particule);
 }
 
-std::vector<Particule> PhysicWorld::get_particules() const {
+std::vector<Particle> PhysicWorld::get_particules() const {
     return this->particules;
 }
 
-std::vector<Particule>& PhysicWorld::get_particules_ref() {
+std::vector<Particle>& PhysicWorld::get_particules_ref() {
     return this->particules;
 }
