c53b57e33bc9fc8c9abe6739046944de171f2040
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-10-23 23:44:43 -0400

    added missing file

---
+++ src/physics_engine/particle_link.cpp
@@ -0,0 +1,13 @@
+#include "../../include/physics_engine/particle_link.hpp"
+
+ParticleLink::ParticleLink(std::shared_ptr<Particle> p1, std::shared_ptr<Particle> p2) :
+    particle1(p1),
+    particle2(p2) {}
+
+ParticleLink::~ParticleLink() {}
+
+void ParticleLink::add_contact(std::vector<ParticleContact>& contacts) {}
+
+float ParticleLink::current_length() {
+    return (this->particle1->get_position() - this->particle2->get_position()).norm();
+}
