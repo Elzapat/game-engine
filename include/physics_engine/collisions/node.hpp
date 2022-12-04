b658bdbd93f36331c98e85d0db985fa69b4544fd
T0mGib <113405723+T0mGib@users.noreply.github.com>
2022-12-04 15:19:10 -0500

    Node completed ( not tested yet )

--- include/physics_engine/collisions/node.hpp
+++ include/physics_engine/collisions/node.hpp
@@ -5,15 +5,15 @@
 
 #include <vector>
 #include <memory>
-#include "rigid_body.hpp"
+#include "particle.hpp"
 
 class Node
 {
 private:
     math::Vector3 center;
     float half_width;
-    std::vector<std::shared_ptr<RigidBody>> object_list;
-    std::vector<std::shared_ptr<Node*>> p_child;
+    std::vector<std::shared_ptr<Particle>> object_list;
+    std::vector<std::shared_ptr<Node>> p_child;
 
 public:
     Node(/* args */);
@@ -21,24 +21,24 @@ public:
     
     math::Vector3 get_center();
     float get_half_width();
-    std::vector<std::shared_ptr<RigidBody>> get_object_list();
-    std::vector<std::shared_ptr<Node*>> get_children();
+    std::vector<std::shared_ptr<Particle>> get_object_list();
+    std::vector<std::shared_ptr<Node>> get_children();
 
     void set_center(math::Vector3 center);
     void set_half_width(float half_width);
-    void set_object_list(std::vector<std::shared_ptr<RigidBody>> object_list);
-    void set_children(std::vector<std::shared_ptr<RigidBody>> children);
+    void set_object_list(std::vector<std::shared_ptr<Particle>> object_list);
+    void set_children(std::vector<std::shared_ptr<Node>> children);
 
-    void add_child(Node* child);
+    void add_child(std::shared_ptr<Node> child);
+
+    void insert_object(std::shared_ptr<Particle> object);
+    void reset_objects();
+
+    void test_all_collisions();
+
+    void process(std::vector<std::shared_ptr<Partcile>> object_list);
 };
 
-
-
-
-
-
-
-
-
+friend shared_ptr<Node> BuildOctree(math::Vector3 center, float half_width, int stop_depth);
 
 #endif
